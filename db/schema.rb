6bc84375f0a8b36fc2a4c4b3f1ae5fa5947149b1
Ameya Akhouri <ameya.akhouri@gmail.com>
2020-03-26 18:57:41 +1100

    Add message and status to favour applications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_25_040838) do
+ActiveRecord::Schema.define(version: 2020_03_26_075527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_03_25_040838) do
     t.bigint "applicant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "message"
+    t.string "status"
     t.index ["applicant_id"], name: "index_favour_applications_on_applicant_id"
     t.index ["favour_id"], name: "index_favour_applications_on_favour_id"
   end

508ab36a3c2b4ab4b1aaa4bdf3c8489bedc4115a
cassandra calescu <casscalescu@gmail.com>
2020-03-29 23:18:36 +1100

    user can see and edit profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   has_many :favours_asked, class_name: 'Favour', foreign_key: 'recipient_id'
   has_many :favours_done, class_name: 'Favour', foreign_key: 'helper_id'
 
+  has_many :reviews
+
   def helper?
     applications = FavourApplication.where(applicant: self)
     applications.length > 0 ? true : false

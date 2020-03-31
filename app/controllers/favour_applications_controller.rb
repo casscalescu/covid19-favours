02c88dbe99e750373cac3cbb5037428b3a38f417
Ameya Akhouri <ameya.akhouri@gmail.com>
2020-03-31 18:44:45 +1100

    Created FavoursApplication Controller

---
+++ app/controllers/favour_applications_controller.rb
@@ -0,0 +1,2 @@
+class FavourApplicationsController < ApplicationController
+end
--- db/migrate/20200331044750_remove_start_end_date_favours.rb
+++
@@ -1,6 +0,0 @@
-class RemoveStartEndDateFavours < ActiveRecord::Migration[5.2]
-  def change
-  	remove_column :favours, :start_date
-  	remove_column :favours, :end_date
-  end
-end

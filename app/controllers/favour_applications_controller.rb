2acb14a226bffeccbdd2b394e78577a19e91e204
Ameya Akhouri <ameya.akhouri@gmail.com>
2020-04-03 13:26:31 +1100

    Controller new, create, destroy. Add conditional buttons to favour index and favour show. Create favour new form.

--- app/controllers/favour_applications_controller.rb
+++ app/controllers/favour_applications_controller.rb
@@ -1,2 +1,37 @@
 class FavourApplicationsController < ApplicationController
+  before_action :set_favour, only: [:new, :create, :index]
+
+  def new
+    @favour_application = FavourApplication.new
+  end
+
+  def create
+    @favour_application = FavourApplication.new(favour_application_params)
+    @favour_application.favour = @favour
+    @favour_application.applicant = current_user
+    @favour_application.status = "Pending"
+    @favour_application.save
+    redirect_to favour_path(@favour)
+  end
+
+  def index
+
+  end
+
+  def destroy
+    @favour_application = FavourApplication.find(params[:id])
+    @favour_application.status = "Withdrawn"
+    @favour_application.save
+    redirect_to favour_path(@favour_application.favour)
+  end
+
+  private
+
+  def set_favour
+    @favour = Favour.find(params[:favour_id])
+  end
+
+  def favour_application_params
+    params.require(:favour_application).permit(:message)
+  end
 end

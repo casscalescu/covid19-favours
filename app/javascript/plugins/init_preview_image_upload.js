4978545fbedd72a0f80d95a2b1a40de48864e7e4
Ameya Akhouri <ameya.akhouri@gmail.com>
2020-05-19 11:56:06 +1000

    Applied styling to log in and sign up forms. Fixed BEM naming for forms.

--- app/javascript/plugins/init_preview_image_upload.js
+++ app/javascript/plugins/init_preview_image_upload.js
@@ -1,16 +1,16 @@
-const name = document.querySelector('.form__upload-file-name');
-const input = document.querySelector('.form__upload-input');
-const label = document.querySelector('.form__upload-label');
-const imgPreview = document.querySelector('.form__upload-image-preview');
+const name = document.querySelector('.form__file-name');
+const input = document.querySelector('.form__input-file');
+const label = document.querySelector('.form__label-upload');
+const imgPreview = document.querySelector('.form__image-preview');
 
 const previewImage = (e) => {
   if(e.target.files[0]) {
-    label.classList.remove('form__upload-label--smiley');
-    label.classList.add('button', 'form__upload-label--change-button');
+    label.classList.remove('form__label-file--smiley');
+    label.classList.add('button', 'form__label-file--change-button');
     label.innerHTML = "Change Photo";
     imgPreview.src = URL.createObjectURL(e.target.files[0]);
     imgPreview.style.display = 'block';
-    name.innerText = `Photo uploaded: ${e.target.files[0].name}`;
+    name.innerText = `${e.target.files[0].name}`;
   }
 }
 
